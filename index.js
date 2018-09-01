d65424c4144629ec42ce43519647a2a59a18bb85
Manju Thomas <kudukacar@hotmail.com>
2018-09-01 18:00:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(mus, inst) {
   var emptyArray = [];
-  for (let i = 0, i < mus.length + 1, i++) {
+  for (let i = 0, i < mus.length, i++) {
 emptyArray.push(mus[i] + inst[i]);    
 }
 Return emptyArray;
@@ -8,5 +8,5 @@ Return emptyArray;
 function johnLennonFacts(facts) {
   var text = "!!!";
   var i = 0;
-  while (i < cars.length)
+  while (i < cars.length) {
 }
